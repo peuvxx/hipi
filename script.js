50b46bd67d48b71be8466282b4174be3ddb5c288
peuvxx <choisubin031210@gmail.com>
2024-10-30 22:21:08 +0900

    hipi7

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ cardInners.forEach((cardInner) => {
 
         // 배경 색상 변경
         const randomHue = Math.random() * 360; // 랜덤 색상 생성
-        const randomSaturation = Math.random() * 50 + 50; // 채도 50%~100%
+        const randomSaturation = Math.random() * 70 + 50; // 채도 50%~100%
         const randomLightness = Math.random() * 30 + 40; // 명도 40%~100%
         const randomColor = `hsl(${randomHue}, ${randomSaturation}%, ${randomLightness}%)`; 
 
@@ -21,7 +21,54 @@ cardInners.forEach((cardInner) => {
         if (!isFlipped) {
             setTimeout(() => {
                 cardInner.style.transform = 'rotateY(0deg)';
-            }, 2500); // 3초 후에 앞면으로 뒤집음
+            }, 1000); // 3초 후에 앞면으로 뒤집음
         }
     });
 });
+
+const images = [
+    'images/h1.png', // 실제 이미지 파일 경로로 변경
+    'images/h2.png', // 실제 이미지 파일 경로로 변경
+    'images/h3.png', // 실제 이미지 파일 경로로 변경
+    'images/h4.png', // 실제 이미지 파일 경로로 변경
+    'images/h5.png', // 실제 이미지 파일 경로로 변경
+    'images/h6.png', // 실제 이미지 파일 경로로 변경
+    'images/h7.png', // 실제 이미지 파일 경로로 변경
+    'images/h8.png', // 실제 이미지 파일 경로로 변경
+    'images/h9.png', 
+    'images/h10.png', 
+    'images/h11.png', 
+    'images/h12.png', 
+    'images/h13.png', 
+    'images/h14.png', 
+    'images/h15.png', 
+    'images/h16.png', 
+    'images/h17.png', 
+    'images/h18.png', 
+    'images/h19.png', 
+  
+];
+
+function createFallingImage() {
+    const image = document.createElement('img');
+    
+    // 배열에서 랜덤한 이미지 선택
+    const randomIndex = Math.floor(Math.random() * images.length);
+    image.src = images[randomIndex]; // 랜덤 이미지 설정
+    
+    image.className = 'falling-image';
+    document.body.appendChild(image); // body에 이미지 추가
+
+    // 랜덤한 위치 생성
+    const randomPosition = Math.random() * (window.innerWidth - 100); // 이미지의 너비를 고려하여 위치 설정
+    image.style.left = randomPosition + 'px'; // x축 랜덤 위치
+
+    // 애니메이션이 끝난 후 5초 후에 사라지도록 설정
+    setTimeout(() => {
+        image.remove(); // 5초 후에 DOM에서 삭제
+    }, 6000); // 애니메이션 시간과 동일하게 설정
+}
+
+// 무한으로 이미지 생성
+setInterval(createFallingImage, 2000); // 2초마다 이미지 생성
+
